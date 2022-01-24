8f5f0e46dc554c7c54febba9b2802826308a1733
Agustin Segovia <agustin.segovia@wes-it.com>
2022-01-24 11:15:32 -0300

    first commit

---
+++ Models/UserContext.cs
@@ -0,0 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+
+public class UserContext : DbContext{
+    public UserContext(DbContextOptions<UserContext> options) : base(options){ }
+    public DbSet<User> Users {get;set;}
+}
